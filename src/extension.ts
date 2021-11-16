e9a8b02685947a3f165dd45d6b7afa1206d7030b
liangzizhou <liangzizhou@youcku.com>
2021-11-16 12:28:24 +0800

    v0.0.1

--- src/extension.ts
+++ src/extension.ts
@@ -2,17 +2,60 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import { workspace } from 'vscode';
+
+const N = "note.turnOn";
+
+const config = workspace.getConfiguration();
+
+const turnOff = () => {
+	const v = config.get("workbench.editorAssociations");
+	// @ts-ignore
+	v["*.md"]="default";
+	config.update("workbench.editorAssociations", v);
+	// config.update("vscode-md.options.mode", "ir");
+};
+
+const turnOn = () => {
+
+	const v = config.get("workbench.editorAssociations");
+	// @ts-ignore
+	v["*.md"]="myEdit.markdown";
+	config.update("workbench.editorAssociations", v);
+	config.update("vscode-md.options.mode", "ir");
+};
+
+const noteModeSwitch = () => {
+	const config = workspace.getConfiguration();
+
+	if (!config.has(N)) { return; }
+
+	const isOpen = config.get(N);
+
+	isOpen ? turnOn() : turnOff();
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('note-pack.turnOn', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from vscode-note-pack!');
+	// 立即检测下配置
+	noteModeSwitch();
+
+	// 监听配置
+	workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
+		if (!e.affectsConfiguration(N)) { return; }
+		noteModeSwitch();
 	});
 
-	context.subscriptions.push(disposable);
+	// 注册命令
+	const turnOnCMD = vscode.commands.registerCommand('note-pack.turnOn', () => {
+		config.update(N, true);
+	});
+
+	const turnOffCMD = vscode.commands.registerCommand('note-pack.turnOff', () => {
+		config.update(N, false);
+	});
+
+	context.subscriptions.push(turnOnCMD, turnOffCMD);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
