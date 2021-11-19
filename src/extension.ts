830e7f08da5ad8d0dc954f103868de5e170d25a5
liangzizhou <liangzizhou@youcku.com>
2021-11-19 18:45:37 +0800

    v0.0.3

--- src/extension.ts
+++ src/extension.ts
@@ -8,49 +8,68 @@ const N = "note.turnOn";
 
 const config = workspace.getConfiguration();
 
-const source:Record<string, unknown> = {};
+const relativeSetting = [
+	"workbench.editorAssociations",
+	"note.turnOn"
+];
 
-// @ts-ignore
-source.workbench = config.inspect("workbench.editorAssociations").workspaceValue;
-
-// @ts-ignore
-source.md = config.inspect("vscode-md.options.mode").workspaceValue;
-
-const turnOff = async () => {
-	console.log( "turnoff",source.md);
-	// @ts-ignore
-	// config.update("workbench.editorAssociations", source.workbench);
-	// config.update("vscode-md.options.mode", source.md);
-};
-
-const turnOn = () => {
-	// @ts-ignore
-	const v = config.inspect("vscode-md.options.mode").workspaceValue||{};
-	// @ts-ignore
-	v["*.md"]="myEdit.markdown";
-	config.update("workbench.editorAssociations", v);
-	config.update("vscode-md.options.mode", "ir");
-};
-
-const noteModeSwitch = async () => {
-	const config = workspace.getConfiguration();
-
-	if (!config.has(N)) { return; }
-
-	const isOpen = config.get(N);
-
-	isOpen ? turnOn() : turnOff().catch(e=>{console.log(e);});
-};
 
 export function activate(context: vscode.ExtensionContext) {
-	// 立即检测下配置
-	noteModeSwitch();
+
+	const workspaceURI = workspace.workspaceFolders?.[0].uri.toString() || '';
+
+	const getHistorySetting = () => context.globalState.get(`${workspaceURI}-settingHistory`) as Record<string, unknown>;
+
+	const setHistorySetting = (n: Record<string, unknown>) => context.globalState.update(`${workspaceURI}-settingHistory`, n);
+
+	if (!getHistorySetting()) {
+		const r: Record<string, unknown> = {};
+		for (const setting of relativeSetting) {
+			r[setting] = config.inspect(setting)?.workspaceValue;
+		}
+		setHistorySetting(r);
+	}
+
+	const turnOff = async () => {
+
+		const config = workspace.getConfiguration();
+		const v = (config.inspect("workbench.editorAssociations")?.workspaceValue || {}) as Record<string, unknown>;
+		Reflect.deleteProperty(v,"*.md");
+		const isEmpty = Object.keys(v).length === 0;
+		console.log(isEmpty);
+		config.update("workbench.editorAssociations", isEmpty ? undefined : v);
+	};
+
+	const turnOn = async () => {
+
+		const config = workspace.getConfiguration();
+		const v = config.inspect("workbench.editorAssociations")?.workspaceValue || {};
+		// @ts-ignore
+		v["*.md"] = "myEdit.markdown";
+		config.update("workbench.editorAssociations", v);
+	};
+
+	const noteModeSwitch = () => {
+		const config = workspace.getConfiguration();
+		if (typeof config.inspect(N)?.workspaceValue === "undefined") { return; }
+
+		const isOpen = config.get(N);
+		isOpen ? turnOn() : turnOff().catch(e => { console.log(e); });
+	};
+
+
+
 
 	// 监听配置
-	workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
-		if (!e.affectsConfiguration(N)) { return; }
-		noteModeSwitch();
+	const configChangeListener = workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
+		if (e.affectsConfiguration(N)) {
+
+			noteModeSwitch();
+		}
 	});
+
+	// 立即检测下配置
+	noteModeSwitch();
 
 	// 注册命令
 	const turnOnCMD = vscode.commands.registerCommand('note-pack.turnOn', () => {
@@ -61,7 +80,7 @@ export function activate(context: vscode.ExtensionContext) {
 		config.update(N, false);
 	});
 
-	context.subscriptions.push(turnOnCMD, turnOffCMD);
+	context.subscriptions.push(turnOnCMD, turnOffCMD, configChangeListener);
 }
 
 // this method is called when your extension is deactivated
